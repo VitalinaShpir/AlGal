a90c10e043bdc4f711029b3cceaacd6128d76291
VitalinaShpir <grigorenko.vitalina@gmail.com>
2023-07-04 13:52:22 +0300

    add galery

---
+++ js/01-gallery.js
@@ -0,0 +1,26 @@
+const galleryList = document.querySelector('.gallery')
+
+galleryList.addEventListener("click", onGalleryItemClick)
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+  if(event.target.nodeName !== 'IMG'){
+    return
+  }
+ 
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" alt="${event.target.alt}">`,
+
+    {onShow: (instance) => {window.addEventListener("keydown", closeByEsc)},
+
+	onClose: (instance) => {window.removeEventListener("keydown", closeByEsc)}
+  })
+ 
+instance.show()
+
+
+function closeByEsc({ code }) {
+  if (code === "Escape") {
+    instance.close();
+  }}}
+
