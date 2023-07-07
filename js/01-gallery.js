b07830e89f1e7d0807c6410f69b3bd4e2cfcdcac
VitalinaShpir <grigorenko.vitalina@gmail.com>
2023-07-07 13:12:07 +0300

    add lib

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,26 +1,35 @@
-const galleryList = document.querySelector('.gallery')
 
-galleryList.addEventListener("click", onGalleryItemClick)
+
+const galleryList = document.querySelector(".gallery");
+
+
+galleryList.addEventListener("click", onGalleryItemClick);
 
 function onGalleryItemClick(event) {
   event.preventDefault();
-  if(event.target.nodeName !== 'IMG'){
-    return
+  if (event.target.nodeName !== "IMG") {
+    return;
   }
- 
+
   const instance = basicLightbox.create(
     `<img src="${event.target.dataset.source}" alt="${event.target.alt}">`,
 
-    {onShow: (instance) => {window.addEventListener("keydown", closeByEsc)},
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", closeByEsc);
+      },
 
-	onClose: (instance) => {window.removeEventListener("keydown", closeByEsc)}
-  })
- 
-instance.show()
+      onClose: (instance) => {
+        window.removeEventListener("keydown", closeByEsc);
+      },
+    }
+  );
 
+  instance.show();
 
-function closeByEsc({ code }) {
-  if (code === "Escape") {
-    instance.close();
-  }}}
-
+  function closeByEsc({ code }) {
+    if (code === "Escape") {
+      instance.close();
+    }
+  }
+}
